041191d72489b164c8cef3ec669f05ba16b337d5
wenjunnie <1287018840@qq.com>
2019-12-19 10:41:38 +0800

    初始化项目

---
+++ src/test/java/com/wenjun/seckill/SeckillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wenjun.seckill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeckillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
