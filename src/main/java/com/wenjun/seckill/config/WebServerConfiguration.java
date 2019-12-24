567286a8f7d62a06f2bd7eaa71cf2ef5ee8bccb3
wenjunnie <1287018840@qq.com>
2019-12-24 17:11:15 +0800

    修改Tomcat配置

---
+++ src/main/java/com/wenjun/seckill/config/WebServerConfiguration.java
@@ -0,0 +1,33 @@
+package com.wenjun.seckill.config;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
+
+/**
+ * 通过此类修改Tomcat参数，性能调优
+ * 当Spring容器内没有TomcatEmbeddedServletContainerFactory这个bean时，会把此bean加载进spring容器中
+ * @Author: wenjun
+ * @Date: 2019/12/24 14:49
+ */
+@Component
+public class WebServerConfiguration implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+        //使用对应工厂类提供给我们的接口定制化我们的Tomcat Connector
+        ((TomcatServletWebServerFactory)factory).addConnectorCustomizers(new TomcatConnectorCustomizer() {
+            @Override
+            public void customize(Connector connector) {
+                Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+                //定制化KeepAliveTimeout,设置30秒内没有请求则服务端自动断开KeepAlive链接
+                protocol.setKeepAliveTimeout(30000);
+                //当客户端发送超过10000个请求则自动断开KeepAlive链接
+                protocol.setMaxKeepAliveRequests(10000);
+            }
+        });
+    }
+}
