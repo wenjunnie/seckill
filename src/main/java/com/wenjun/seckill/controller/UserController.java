9eb9ebbc6ff7362be8c17a9c0ba979c585cb8514
wenjunnie <1287018840@qq.com>
2019-12-25 20:15:44 +0800

    新增阿里云短信验证

--- src/main/java/com/wenjun/seckill/controller/UserController.java
+++ src/main/java/com/wenjun/seckill/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.wenjun.seckill.controller;
 
+import com.aliyuncs.exceptions.ClientException;
 import com.wenjun.seckill.controller.viewobject.UserVO;
 import com.wenjun.seckill.enums.EmBusinessError;
 import com.wenjun.seckill.error.BusinessException;
 import com.wenjun.seckill.response.CommonReturnType;
 import com.wenjun.seckill.service.impl.UserServiceImpl;
 import com.wenjun.seckill.service.model.UserModel;
+import com.wenjun.seckill.util.AliyunMessageUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +18,6 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Random;
 
 /**
  * @Author: wenjun
@@ -46,16 +47,19 @@ public class UserController {
 
     //用户获取OTP短信接口
     @GetMapping(value = "/getotp")
-    public CommonReturnType getOtp(@RequestParam(name = "telphone") String telphone) {
+    public CommonReturnType getOtp(@RequestParam(name = "telphone") String telphone) throws ClientException {
         //生成otp验证码
-        Random random = new Random();
-        int randomInt = random.nextInt(99999);
-        randomInt += 10000;
-        String otpCode = String.valueOf(randomInt);
+//        Random random = new Random();
+//        int randomInt = random.nextInt(99999);
+//        randomInt += 10000;
+//        String otpCode = String.valueOf(randomInt);
+        String otpCode = AliyunMessageUtil.createRandomNum(6);
         //将otp验证码与手机号关联（通过session）
-        httpServletRequest.getSession().setAttribute(telphone,otpCode);
+        httpServletRequest.getSession().setAttribute("telphone",telphone);
+        httpServletRequest.getSession().setAttribute("otpCode",otpCode);
         //将otp验证码通过短信发送给用户，省略
-        System.out.println("telphone= " + telphone + "otpCode= " + otpCode);
+        AliyunMessageUtil.sendMsg(telphone,otpCode);
+        System.out.println("telphone= " + telphone + " otpCode= " + otpCode);
         return CommonReturnType.create("验证码发送成功");
     }
 
@@ -67,7 +71,7 @@ public class UserController {
                                      @RequestParam(name = "gender") Byte gender,
                                      @RequestParam(name = "age") Integer age,
                                      @RequestParam(name = "password") String password) throws BusinessException, UnsupportedEncodingException, NoSuchAlgorithmException {
-        String inSessionOtpCode = (String) httpServletRequest.getSession().getAttribute(telphone);
+        String inSessionOtpCode = (String) httpServletRequest.getSession().getAttribute("otpCode");
         if (!StringUtils.equals(otpCode,inSessionOtpCode)) {
             throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR,"短信验证码不符合");
         }
---
+++ src/main/java/com/wenjun/seckill/util/AliyunMessageUtil.java
@@ -0,0 +1,77 @@
+package com.wenjun.seckill.util;
+
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsRequest;
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
+import com.aliyuncs.profile.DefaultProfile;
+import com.aliyuncs.profile.IClientProfile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 阿里云短信验证
+ * @Author: wenjun
+ * @Date: 2019/12/25 18:03
+ */
+public class AliyunMessageUtil {
+    private final static Logger logger = LoggerFactory.getLogger(AliyunMessageUtil.class);
+
+    private static final String product = "Dysmsapi";
+    //产品域名,开发者无需替换
+    private static final String domain = "dysmsapi.aliyuncs.com";
+
+    // 此处需要替换成开发者自己的AK(在阿里云访问控制台寻找)
+    private static final String accessKeyId = "LTAIHH8KHkrNZnmb";
+    private static final String accessKeySecret = "ekJgBBIdrT4R5PMNSFfSbrGuYXg0o1";
+
+    public static SendSmsResponse sendSms(Map<String, String> paramMap) throws com.aliyuncs.exceptions.ClientException {
+
+        //可自助调整超时时间
+        System.setProperty("sun.net.client.defaultConnectTimeout", "10000");
+        System.setProperty("sun.net.client.defaultReadTimeout", "10000");
+
+        //初始化acsClient,暂不支持region化
+        IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);
+        DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);
+        IAcsClient acsClient = new DefaultAcsClient(profile);
+
+        //组装请求对象-具体描述见控制台-文档部分内容
+        SendSmsRequest request = new SendSmsRequest();
+        //必填:待发送手机号
+        request.setPhoneNumbers(paramMap.get("phoneNumber"));
+        //必填:短信签名-可在短信控制台中找到
+        request.setSignName(paramMap.get("msgSign"));
+        //必填:短信模板-可在短信控制台中找到
+        request.setTemplateCode(paramMap.get("templateCode"));
+        //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
+        request.setTemplateParam(paramMap.get("jsonContent"));
+
+        //hint 此处可能会抛出异常，注意catch
+        SendSmsResponse sendSmsResponse = acsClient.getAcsResponse(request);
+        return sendSmsResponse;
+    }
+    //通过阿里云发送短信验证码
+    public static void sendMsg(String uphone,String randomNum) throws com.aliyuncs.exceptions.ClientException {
+        String jsonContent = "{\"code\":\"" + randomNum + "\"}";
+        Map<String, String> paramMap = new HashMap<>();
+        paramMap.put("phoneNumber", uphone);
+        paramMap.put("msgSign", "知你点评");
+        paramMap.put("templateCode", "SMS_130795159");
+        paramMap.put("jsonContent", jsonContent);
+        SendSmsResponse sendSmsResponse = sendSms(paramMap);
+    }
+
+    //生成num位随机数
+    public static String createRandomNum(int num){
+        String randomNumStr = "";
+        for(int i = 0; i < num;i ++){
+            int randomNum = (int)(Math.random() * 10);
+            randomNumStr += randomNum;
+        }
+        return randomNumStr;
+    }
+}
